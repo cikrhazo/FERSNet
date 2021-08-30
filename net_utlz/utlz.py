810806176734da1df2c1cfafcad789823dc58abc
cikrhazo <75162193+cikrhazo@users.noreply.github.com>
2021-08-30 12:44:27 +0800

    Create utlz.py

---
+++ net_utlz/utlz.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+
+
+def initialize_weights_xavier(net_l, scale=1):
+    if not isinstance(net_l, list):
+        net_l = [net_l]
+    for net in net_l:
+        for m in net.modules():
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+                init.xavier_normal_(m.weight)
+                m.weight.data *= scale  # for residual block
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                init.xavier_normal_(m.weight)
+                m.weight.data *= scale
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                init.constant_(m.weight, 1)
+                init.constant_(m.bias.data, 0.0)
